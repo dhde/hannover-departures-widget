d855659c639a426550ce956b96dd8a460a24c14f
Dietrich Heise <dh@dhde.de>
2026-06-02 14:18:29 +0200

    feat: relase v1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.dhde.hannover.departures.widget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.5.2"
+        versionCode = 20
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
